ac1def2e274aefa838a703cf2c345ec4c02423e0
Anne Vollmann <vollmann.anne@web.de>
2026-03-06 19:30:37 +0100

    feat: task interface

---
+++ src/types/Task.ts
@@ -0,0 +1,10 @@
+export type Priority = "Niedrig" | "Mittel" | "Hoch";
+
+export interface Task {
+  id: string;
+  name: string;
+  complexity: 1 | 2 | 3 | 4 | 5;
+  priority: Priority;
+  priorityScore: number;
+  completed: boolean;
+}
