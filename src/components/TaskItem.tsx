b74f079113bc533139c97817cb0338f16d0be173
Anne Vollmann <vollmann.anne@web.de>
2026-03-07 19:00:31 +0100

    style: TaskItem Design

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -8,19 +8,29 @@ import {
     Checkbox,
     Button
 } from "@mui/material";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 export default function TaskItem({ task, onDelete, onToggleComplete }: TaskItemProps) {
+    const highPriority = task.priorityScore > 10;
+
     return (
-        <Card>
+        <Card
+            sx={{
+                opacity: task.completed ? 0.5 : 1,
+                border: highPriority ? "4px solid #f44336" : "4px solid #fff"
+            }}
+        >
             <CardContent>
-                <Typography>{task.name}</Typography>
+                <Typography sx={{fontWeight: "bold", fontSize: 20}}>{task.name}</Typography>
                 <Typography>Komplexität: {task.complexity}</Typography>
                 <Typography>Priorität: {task.priority}</Typography>
                 <Typography>Score: {task.priorityScore}</Typography>
             </CardContent>
-            <CardActions>
+            <CardActions sx={{gap: "8px"}}>
                 <Checkbox checked={task.completed} onChange={() => onToggleComplete(task.id)} />
-                <Button onClick={() => onDelete(task.id)}>Delete</Button>
+                <Button onClick={() => onDelete(task.id)} sx={{aspectRatio:1, minWidth: "unset", borderRadius:"50%"}}>
+                    <DeleteForeverIcon sx={{aspectRatio:1}}/>
+                </Button>
             </CardActions>
         </Card >
     )
